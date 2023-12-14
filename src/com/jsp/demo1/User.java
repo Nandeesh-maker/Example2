368f80b0ede6d9a24a563d4c2df2cbd0ab36e75a
Nandeesh-maker <nandeeshmdoggalli@gmail.com>
2023-12-14 15:02:09 +0530

    added

---
+++ src/com/jsp/demo1/User.java
@@ -0,0 +1,28 @@
+package com.jsp.demo1;
+
+public class User {
+
+	private String name;
+	private String gmail;
+	private long contact;
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGmail() {
+		return gmail;
+	}
+	public void setGmail(String gmail) {
+		this.gmail = gmail;
+	}
+	
+	
+}
